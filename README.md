4db1ca64db953862b4bf9eb37398e581078518c7
faccordoba <facundosebastian.cordoba@mercadolibre.com>
2023-01-07 10:07:24 -0300

    commit initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# data-binding-and-thymeleaf
+4 Sesion   spring boot y spring framework
---
+++ src/main/java/com/bolsadeideas/form/app/controllers/FormController.java
@@ -0,0 +1,63 @@
+package com.bolsadeideas.form.app.controllers;
+
+import com.bolsadeideas.form.app.model.Usuario;
+import jakarta.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class FormController {
+
+  @GetMapping("/form")
+  public String form(Model model) {
+    Usuario usuario = new Usuario();
+    model.addAttribute("titulo", "formulario de usuario");
+    model.addAttribute("usuario", usuario);
+    return "form";
+  }
+
+  @PostMapping("/resultado")
+  public String procesorForm(@Valid Usuario usuario, BindingResult result, Model model) {
+    model.addAttribute("usuario", usuario);
+    if (result.hasErrors()) {
+      Map<String, String> errores = new HashMap<>();
+      result.getFieldErrors().forEach(error -> {
+        errores.put(error.getField(), "Campo: ".concat(error.getField())
+            .concat(" ").concat(error.getDefaultMessage()));
+      });
+      model.addAttribute("error", errores);
+      return "form";
+    }
+    return "resultado";
+  }
+
+
+/*
+  @PostMapping("/resultado")
+  public String procesorForm(Model model,
+                             @RequestParam String username,
+                             @RequestParam String password,
+                             @RequestParam String email, @RequestParam Map<String, String> params) {
+
+    Usuario usuario = new Usuario();
+    usuario.setUsername(username);
+    usuario.setPassword(password);
+    usuario.setEmail(email);
+
+    model.addAttribute("usuario", usuario);
+
+    model.addAttribute("titulo", "resultado Form");
+    model.addAttribute("username_1", username);
+    model.addAttribute("password_1", password);
+    model.addAttribute("email_1", email);
+    model.addAllAttributes(params);
+    return "resultado";
+  }
+
+*/
+}
